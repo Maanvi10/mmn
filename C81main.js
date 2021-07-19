a380fdd0d1760c74b60a6f6c86e40cb321490077
Maanvi10 <82942335+Maanvi10@users.noreply.github.com>
2021-07-19 19:40:37 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,31 @@
+canvas =document.getElementById("Canvas1");
+ctx= canvas.getContext("2d");
+color = "deeppink";
+width=2;
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = width;
+ctx.arc(400, 300, 40, 0, 2 * Math.PI)
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e)
+{
+    
+
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+
+    console.log("X ="+mouse_x +" ,Y =" +mouse_y);
+    circle(mouse_x , mouse_y);
+}
+
+function circle(mouse_x , mouse_y)
+{
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+    ctx.arc(mouse_x, mouse_y, 40, 0, 2 * Math.PI)
+    ctx.stroke();
+}
